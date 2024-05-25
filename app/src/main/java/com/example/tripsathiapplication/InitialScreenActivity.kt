531470061cc255510298a747c01e4f5483640065
Aku-12 <akashchaudhary205698@gmail.com>
2024-05-25 18:36:42 +0545

    initial screen activity and xml file added

---
+++ app/src/main/java/com/example/tripsathiapplication/InitialScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tripsathiapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InitialScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_initial_screen)
+        val openSplashButton: Button = findViewById(R.id.button)
+        openSplashButton.setOnClickListener {
+            val intent = Intent(this@InitialScreenActivity, SplashScreenActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
