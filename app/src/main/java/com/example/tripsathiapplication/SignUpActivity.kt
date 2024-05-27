1e615594e3c9d7929892436b123b848fc9c53dbf
Basanta <khandbasanta55@gmail.com>
2024-05-27 20:07:39 +0545

    signup layout design done

---
+++ app/src/main/java/com/example/tripsathiapplication/SignUpActivity.kt
@@ -0,0 +1,19 @@
+package com.example.tripsathiapplication
+
+import android.os.Bundle
+
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a34a.R
+
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+    }
+}
